f9aa5f3120590010fdac1cd67908b52a50e78f67
JJ Kim <jj@haedosa.xyz>
2023-02-19 11:25:46 +0900

    simplify nixosConfigurations using __listToAttrs

--- nixosConfigurations/default.nix
+++ nixosConfigurations/default.nix
@@ -4,56 +4,22 @@ let
   inherit (inputs.nixpkgs.lib) nixosSystem;
   specialArgs = { inherit inputs; };
 
-in
-{
-  urubamba = nixosSystem {
-    inherit specialArgs;
-    modules = [ ./hosts/urubamba ];
-  };
+  nixosSystems = __listToAttrs (map (name: {
+    inherit name;
+    value = nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [ ./hosts/${name} ];
+    };
+  }) [ "urubamba"
+       "lima"
+       "lapaz"
+       "bogota"
+       "antofagasta"
+       "giron"
+       "iguazu"
+       "garganta"
+       "usb"
+       "havana"
+     ]);
 
-  lima = nixosSystem {
-    inherit specialArgs;
-    modules = [ ./hosts/lima ];
-  };
-
-  lapaz = nixosSystem {
-    inherit specialArgs;
-    modules = [ ./hosts/lapaz ];
-  };
-
-  bogota = nixosSystem {
-    inherit specialArgs;
-    modules = [ ./hosts/bogota ];
-  };
-
-  antofagasta = nixosSystem {
-    inherit specialArgs;
-    modules = [ ./hosts/antofagasta ];
-  };
-
-  giron = nixosSystem {
-    inherit specialArgs;
-    modules = [ ./hosts/giron ];
-  };
-
-  # iguazu is for a USB installer
-  iguazu = nixosSystem {
-    inherit specialArgs;
-    modules = [ ./hosts/iguazu ];
-  };
-
-  garganta = nixosSystem {
-    inherit specialArgs;
-    modules = [ ./hosts/garganta ];
-  };
-
-  usb = nixosSystem {
-    inherit specialArgs;
-    modules = [ ./hosts/usb ];
-  };
-
-  havana = nixosSystem {
-    inherit specialArgs;
-    modules = [ ./hosts/havana ];
-  };
-}
+in nixosSystems
