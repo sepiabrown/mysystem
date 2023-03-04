a9118bdf4b0c0a309274edcf5650df26b9160e4d
Suwon Park <sepiabrown@naver.com>
2023-03-04 16:09:04 +0900

    add commonground directory

--- nixosConfigurations/users/suwonp.nix
+++ nixosConfigurations/users/suwonp.nix
@@ -1,11 +1,12 @@
 {
-
-  users.extraUsers.suwonp = {
+  users.groups.commonground = {};
+  users.users.suwonp = {
     isNormalUser = true;
     home = "/home/suwonp";
-    extraGroups = [ "networkmanager" "input" "uinput" "data" ];
+    extraGroups = [ "networkmanager" "input" "uinput" "data" "commonground" ];
     hashedPassword = "$6$NVPZ8mxP39xJFpqi$fvFq/WTIJG62.AlcZftP29.xjTK1r6FP5re89OtyIX8nAk8PwfiEQTzdiWkITAP83fPH8p8csF3G7EnOJMWPN1";
-    openssh.authorizedKeys.keys = [];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO8oBIuYKU4sdtNsDd1WYqILQsHEEGrTw1oYY6APJejr sepiabrown@naver.com"
+    ];
   };
-
 }
